cbaea857bfa15111b1b891e56c8b4b5bc0040e98
sarah <sarahphan2314@gmail.com>
2021-11-07 10:16:56 +0700

    Scrolling header

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var scrollpos = window.scrollY;
+var header = document.getElementById("headerMenu");
+
+function add_class_on_scroll() {
+    header.classList.add("navbar__scroll");
+    document.getElementById("logo").style.display = "none";
+}
+
+function remove_class_on_scroll() {
+    header.classList.remove("navbar__scroll");
+    document.getElementById("logo").style.display = "block";
+}
+
+window.addEventListener('scroll', function(){ 
+    //Here you forgot to update the value
+    scrollpos = window.scrollY;
+
+    if(scrollpos > 100){
+        add_class_on_scroll();
+    }
+    else {
+        remove_class_on_scroll();
+    }
+    console.log(scrollpos);
+});
